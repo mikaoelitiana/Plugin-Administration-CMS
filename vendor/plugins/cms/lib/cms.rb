06738bb99129c501356b999390779471c386d93d
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2011-09-22 20:02:11 +0300

    Ajout des controllers, models, helpers et views

--- vendor/plugins/cms/lib/cms.rb
+++ vendor/plugins/cms/lib/cms.rb
@@ -1,1 +1,10 @@
 # Cms
+#chargement des modeles, controleurs
+%w{ models controllers helpers views}.each do |dir|
+path = File.join(File.dirname(__FILE__), 'app', dir)
+$LOAD_PATH << path
+ActiveSupport::Dependencies.load_paths << path
+ActiveSupport::Dependencies.load_once_paths.delete(path)
+end
+#requisition des routs
+require "cms/routing.rb"
--- vendor/plugins/cms/test/test_helper.rb
+++ vendor/plugins/cms/test/test_helper.rb
@@ -1,3 +1,10 @@
+#fichiers requis
 require 'rubygems'
 require 'test/unit'
 require 'active_support'
+require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config/environment'
+#Variables d'environnement
+ENV['RAILS_ENV'] = 'test'
+ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '/../../../..'
+
+
